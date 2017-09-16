46877d7906b59bd55e37dd282aac6c560ac82bdd
alzaslon <alzaslon@microsoft.com>
2017-09-16 10:34:57 -0700

    tmp

--- src/editing/IWidgetOrder.ts
+++ src/editing/IWidgetOrder.ts
@@ -3,7 +3,8 @@ import { IWidgetFactoryResult } from '../editing/IWidgetFactoryResult';
 import { IWidgetOrder } from '../editing/IWidgetOrder';
 
 export interface IWidgetOrder { //to be displayed in UI and enough to build new HTML element
-    title: string;
+    name: string;
+    displayName: string;
     createWidget(): IWidgetFactoryResult;
     createModel(): any;
 }
