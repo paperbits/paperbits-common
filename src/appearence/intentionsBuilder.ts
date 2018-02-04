7a0d3449fe9841d6035d0a8e82cb47482202b8dc
mtuchkov <tuchkov.michail@gmail.com>
2018-02-04 02:34:42 -0800

    remove viewports level from intentions

--- src/appearence/intentionsBuilder.ts
+++ src/appearence/intentionsBuilder.ts
@@ -18,82 +18,108 @@ export class Interface {
 
 /// TODO: Consider immutable; consider to split code generator and intentions builder
 export class IntentionsBuilder implements IIntentionsBuilder {
-        constructor(private theme: any,
-            private interfaceDefinition?: Interface,
-            private interfaceDefinitions?: Interface[])
-        {
-            if (!interfaceDefinition){
-                this.interfaceDefinition = new Interface("Root");
-            }
-            if (!interfaceDefinitions){
-                this.interfaceDefinitions = new Array<Interface>();
-            }
-        }
     
-        private intentions : IntentionsMap = <IntentionsMap>{};
-        private prefix: string;
+    private static viewPorts = ["xs", "sm", "md", "lg", "xl"];
+        
+    constructor(private theme: any,
+        private interfaceDefinition?: Interface,
+        private interfaceDefinitions?: Interface[])
+    {
+        if (!interfaceDefinition){
+            this.interfaceDefinition = new Interface("Root");
+        }
+        if (!interfaceDefinitions){
+            this.interfaceDefinitions = new Array<Interface>();
+        }
+    }
+
+    private intentions : IntentionsMap = <IntentionsMap>{};
+    private prefix: string;
+    private viewportContainer: boolean;
     
     public addIntention(
             path: string, 
             category: string,
             name: string,
-            scope: string,
-            viewport?: string): IIntentionsBuilder{
+            scope: string): IIntentionsBuilder{
             
-            const lastSegment: string = path;
-            path = this.combinePath(path);
-            const intention : Intention= this.buildHierarchy(path);
-            const cssClass = this.findClass(path, viewport);
-            intention.category = category;
-            intention.name= () => name;
-            intention.scope= scope;
-            intention.params= () => cssClass;
-            intention.id= lastSegment;
-            intention.fullId= path;
-
-            this.interfaceDefinition.Intentions.push(intention);
-
-            return this;
+        if (this.viewportContainer === true){
+            this.addIntentionPerViewPort(path, category, name, scope);
+        } else {
+            this.addIntentionInternal(path, category, name, scope);
         }
+
+        return this;
+    }
         
-        public addIntentionPerViewPort(path: string, 
-            category: string,
-            name: string,
-            scope: string): IIntentionsBuilder {
+    public addIntentionPerViewPort(
+        path: string, 
+        category: string,
+        name: string,
+        scope: string): IIntentionsBuilder {
 
-            const parent = this.buildHierarchy(this.prefix);
-            const intentionSupplier = (vp) => parent["viewports"][vp][path];
-            
-            this.addIntention(path, category, name, scope);
-            
-            const current = parent[path];
-            current["for"] = intentionSupplier;
-            
-            this.scope("viewports", viewportsBuilder=> {
-                const viewPorts = ["xs", "sm", "md", "lg", "xl"];
-                viewPorts.forEach(viewPort => {
-                    viewportsBuilder.scope(viewPort, viewportBuilder => {
-                        return viewportBuilder.addIntention(path, category, name, scope, viewPort);
-                    })
-                });
-                return viewportsBuilder;
-            }); 
-            return this;
-        }
+        const parent = this.buildHierarchy(this.prefix);
+        const intentionSupplier = (vp) => parent[vp][path];
+        
+        this.addIntentionInternal(path, category, name, scope);
+        
+        const current = parent[path];
+        current["for"] = intentionSupplier;
+        
+        IntentionsBuilder.viewPorts.forEach(viewPort => {
+            this.scope(viewPort, viewportBuilder => {
+                return viewportBuilder.addIntentionInternal(path, category, name, scope, viewPort);
+            })
+        });
+        return this;
+    }
+
+    private addIntentionInternal(
+        path: string, 
+        category: string,
+        name: string,
+        scope: string,
+        viewport?: string): IIntentionsBuilder {
+        
+        const lastSegment: string = path;
+        path = this.combinePath(path);
+        const intention : Intention= this.buildHierarchy(path);
+        const cssClass = this.findClass(path, viewport);
+        intention.category = category;
+        intention.name= () => name;
+        intention.scope= scope;
+        intention.params= () => cssClass;
+        intention.id= lastSegment;
+        intention.fullId= path;
+
+        this.interfaceDefinition.Intentions.push(intention);
+
+        return this;
+    }
         
         public scope(
             prefix: string, 
-            buildAction: (IntentionsBuilder) => IIntentionsBuilder): IIntentionsBuilder {
+            buildAction: (IntentionsBuilder) => IIntentionsBuilder,
+            viewportContainer: boolean = false): IIntentionsBuilder {
+
+            if (this.viewportContainer && viewportContainer){
+                throw new Error("Nested viewport container scopes are not allowed.")
+            }
+
             //save
             const currentPrefix = this.prefix;
             const currentInterfaceDefinition = this.interfaceDefinition;
+            const currentViewportContainer = this.viewportContainer;
             this.prefix = this.combinePath(prefix);
+            this.viewportContainer = viewportContainer;
 
             this.interfaceDefinition = currentInterfaceDefinition.Scopes.find(scope => scope.Name === this.prefix);
             if (!this.interfaceDefinition)
             {
-                this.interfaceDefinition = new Interface(this.prefix, this.prefix.endsWith("viewports"));
-                currentInterfaceDefinition.Scopes.push(this.interfaceDefinition);
+                const newInterfaceDefinition = new Interface(this.prefix, viewportContainer);
+                currentInterfaceDefinition.Scopes.push(newInterfaceDefinition);
+                this.interfaceDefinition = newInterfaceDefinition;
+                
             }
             
             //build internal
@@ -102,6 +128,7 @@ export class IntentionsBuilder implements IIntentionsBuilder {
             //restore
             this.interfaceDefinition = currentInterfaceDefinition;
             this.prefix = currentPrefix;
+            this.viewportContainer = currentViewportContainer;
             return result;
         }
     
@@ -116,15 +143,14 @@ export class IntentionsBuilder implements IIntentionsBuilder {
             let contracts : string = "";
             contracts += "/********************************************************************\n"; 
             contracts += "THIS IS AUTO-GENERATED CODE.\n";
-            contracts += "DO NOT MODIFY THIS FILE DIRECTLY OTHERWISE ALL CHANGES WILL BE LOST.\n";
+            contracts += "DO NOT MODIFY THIS FILE MANUALLY, OTHERWISE ALL CHANGES WILL BE LOST.\n";
             contracts += "********************************************************************/\n"; 
             contracts +="\nimport { Intention, IntentionsMap, IntentionWithViewport } from '@paperbits/common/appearence/intention'\n";
             
             contracts = this.appendInterface(contracts, this.interfaceDefinition);
 
             return contracts;
-        }
-        
+        }        
 
         private buildHierarchy(path: string) : any {
             const segments: Array<string> = path.split(".");
@@ -145,16 +171,13 @@ export class IntentionsBuilder implements IIntentionsBuilder {
             const segments: Array<string> = path.split(".");
             let node: Object = this.theme;
             for(var i = 0; i < segments.length; i++){
-                if (viewport){
-                    if (i === segments.length - 3) {
-                        if (!node["viewports"] || segments.length < i + 3){
-                            return "";
-                        }
-                        return (<string>node["viewports"][segments[i + 2]]).replace("%viewport%", viewport);
-                    }
+                const segment = segments[i];
+                if (IntentionsBuilder.viewPorts.find(vp => vp === segment)){
+                    const cssClass = (<string>node["viewports"][segments[i + 1]]).replace("%viewport%", segment);
+                    return cssClass;
                 }
-                if (node[segments[i]]){
-                    node = node[segments[i]];
+                if (node[segment]){
+                    node = node[segment];
                 }
                 else {
                     return "";
@@ -176,7 +199,7 @@ export class IntentionsBuilder implements IIntentionsBuilder {
             interfaceName = this.replaceAll(interfaceName,".", "_");
             stringBuilder += "\nexport interface " + interfaceName + " extends IntentionsMap{\n";
 
-            definition.Scopes.filter(s => !s.IsViewportContainer).forEach(scope => {
+            definition.Scopes.forEach(scope => {
                 let fieldName = scope.Name.substring(scope.Name.lastIndexOf(".") + 1);
                 stringBuilder += 
                     "\t" + 
@@ -185,8 +208,7 @@ export class IntentionsBuilder implements IIntentionsBuilder {
                     this.replaceAll(this.replaceAll(scope.Name, ".", "_"), "-", "_") + 
                     ";\n";
             });
-            const hasViewports = !!definition.Scopes.find(_ => _.IsViewportContainer);
-            const intentionClassName = hasViewports ? "IntentionWithViewport" : "Intention";
+            const intentionClassName = definition.IsViewportContainer ? "IntentionWithViewport" : "Intention";
 
             definition.Intentions.forEach(intention => {
                 stringBuilder += 
@@ -196,7 +218,7 @@ export class IntentionsBuilder implements IIntentionsBuilder {
             });
             stringBuilder += "}\n";
 
-            definition.Scopes.filter(s => !s.IsViewportContainer).forEach(scope => {
+            definition.Scopes.forEach(scope => {
                 stringBuilder = this.appendInterface(stringBuilder, scope);
             });
 
