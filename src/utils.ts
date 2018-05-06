d88d5a561ebb8d99f565024dbf6f5a9433d0c59a
azaslonov <zaslonov.alexander@gmail.com>
2018-05-05 21:26:16 -0700

    a fix for elementsFromPoint function.

--- src/utils.ts
+++ src/utils.ts
@@ -398,11 +398,11 @@ export function findNodesRecursively(predicate: (x: Object) => boolean, source: 
 }
 
 export function elementsFromPoint(ownerDocument: Document, x: number, y: number): HTMLElement[] {
-    if (ownerDocument["elementsFromPoint"]) {
-        return Array.prototype.slice.call(ownerDocument["elementsFromPoint"](x, y));
+    if (ownerDocument.elementsFromPoint) {
+        return Array.prototype.slice.call(ownerDocument.elementsFromPoint(Math.floor(x), Math.floor(y)));
     }
     else if (ownerDocument.msElementsFromPoint) {
-        return Array.prototype.slice.call(ownerDocument.msElementsFromPoint(x, y));
+        return Array.prototype.slice.call(ownerDocument.msElementsFromPoint(Math.floor(x), Math.floor(y)));
     }
     else {
         throw `Method "elementsFromPoint" not supported by browser.`
