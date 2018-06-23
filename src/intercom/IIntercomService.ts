168f6f58e133d723b3760278d611660bf0716018
Igor O <ygrik@hotmail.com>
2018-06-22 23:20:43 -0700

    moved intercome and textblock to core package

--- src/intercom/IIntercomService.ts
+++
@@ -1,5 +0,0 @@
-import { IIntercomLead } from '../intercom/IIntercomLead';
-
-export interface IIntercomService {
-    update(data: IIntercomLead): void;
-}
--- src/widgets/textblock/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./textblockModel";
-export * from "./textblockModelBinder";
