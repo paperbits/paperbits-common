168251e9f5e825907037391757b735a59b8aae5d
azaslonov <zaslonov.alexander@gmail.com>
2018-08-27 23:36:12 -0700

    added JS docs to component interfaces

--- src/ui/IComponent.ts
+++ src/ui/IComponent.ts
@@ -1,5 +1,19 @@
-﻿export interface IComponent {
+﻿/**
+ * Definition of a UI component.
+ */
+export interface IComponent {
+    /**
+     * Component name.
+     */
     name: string;
+
+    /**
+     * Constructor parameters.
+     */
     params?: any;
+
+    /**
+     * A lifecycle hook fired after the component mounted.
+     */
     oncreate?: (viewModel?, element?) => void;
 }
--- src/ui/IView.ts
+++ src/ui/IView.ts
@@ -1,8 +1,14 @@
 import { IComponent } from "./IComponent";
 
 export interface IView {
+    /**
+     * View heading, e.g. "Picture editor".
+     */
     heading?: string;
     
+    /**
+     * Definition of a UI component behind this view.
+     */
     component: IComponent;
 
     /**
