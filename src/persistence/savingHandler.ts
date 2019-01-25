492bf1133fcaba990ee27bfb65a7c34d521ad4c7
azaslonov <zaslonov.alexander@gmail.com>
2019-01-25 11:47:59 -0800

    removed unnecessary dependencies

--- src/persistence/savingHandler.ts
+++ src/persistence/savingHandler.ts
@@ -4,7 +4,7 @@ import { IObjectStorage } from "./IObjectStorage";
 
 
 export class SavingHandler {
-    constructor(eventManager: IEventManager, viewManager: IViewManager, objectStorage: IObjectStorage) {
+    constructor() {
         // eventManager.addEventListener("onSaveChanges", () => {
         //     const promise = objectStorage.saveChanges();
         //     viewManager.addPromiseProgressIndicator(promise, "Changes saved", "All changes were pushed to server");
