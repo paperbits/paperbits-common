f2f186505e83cdd3ea728be17b087db98d706c58
azaslonov <zaslonov.alexander@gmail.com>
2019-02-15 15:53:31 -0800

    prevent hints from piling up

--- src/tutorials/hinter.ts
+++ src/tutorials/hinter.ts
@@ -2,14 +2,23 @@ import { IViewManager } from "./../ui/IViewManager";
 import { IEventManager } from "../events";
 
 export class Hinter {
+    private noHints: boolean;
+
     constructor(
         private readonly eventManager: IEventManager,
         private readonly viewManager: IViewManager
     ) {
+        this.noHints = false;
         this.eventManager.addEventListener("DesignTimeNavigationHint", this.showDesignTimeNavigationHint.bind(this));
     }
 
     private showDesignTimeNavigationHint(): void {
-         this.viewManager.notifyInfo("Did you know?", `When you're in the administrative view, you still can navigate any website hyperlinks by holding CTRL or ⌘ key and clicking on it.`);
+        if (this.noHints) {
+            return;
+        }
+
+        this.noHints = true;
+        this.viewManager.notifyInfo("Did you know?", `When you're in the administrative view, you still can navigate any website hyperlinks by holding CTRL or ⌘ key and clicking on it.`);
+        setTimeout(() => this.noHints = false, 8000);
     }
 }
