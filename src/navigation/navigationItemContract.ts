1223af4ccec5b468d39326de8f1929dcc90d1092
Mikhail Tuchkov <mikhail.tuchkov@oracle.com>
2019-02-16 17:50:39 -0800

    linux filenames bugfix

---
+++ src/navigation/navigationItemContract.ts
@@ -0,0 +1,24 @@
+﻿/**
+ * Data structure describing a navigation items, e.g. menus.
+ */
+export interface NavigationItemContract {
+    /**
+     * Own key.
+     */
+    key: string;
+
+    /**
+     * Navigation item label.
+     */
+    label: string;
+
+    /**
+     * Key of a permalink referencing resource this navigation item is pointing to.
+     */
+    targetKey?: string;
+
+    /**
+     * Child navigation items.
+     */
+    navigationItems?: NavigationItemContract[];
+}
