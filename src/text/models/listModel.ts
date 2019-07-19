2a3a61b80fb6824148f44b9269d32d18a30fa522
azaslonov <zaslonov.alexander@gmail.com>
2019-07-18 23:53:27 -0700

    Added jsdoc to style compiler interface.

--- src/text/models/listModel.ts
+++ src/text/models/listModel.ts
@@ -2,12 +2,6 @@ import { BlockModel } from "./blockModel";
 import { ListItemModel } from "./listItemModel";
 
 export class ListModel extends BlockModel {
-    public attrs?: {
-        order?: number;
-        styles?: object;
-        className?: string;
-    };
-
     public nodes: ListItemModel[];
 
     constructor(type: string) { 
