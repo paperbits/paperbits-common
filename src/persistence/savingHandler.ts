715ee2ed8a4f3a00e7f2d390c28a05e63b4954f6
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2019-08-27 09:32:58 -0700

    A fix for saving changes notification.

--- src/persistence/savingHandler.ts
+++ src/persistence/savingHandler.ts
@@ -6,8 +6,8 @@ import { OfflineObjectStorage } from ".";
 export class SavingHandler {
     constructor(eventManager: IEventManager, offlineObjectStorage: OfflineObjectStorage, viewManager: IViewManager) {
         eventManager.addEventListener("onSaveChanges", async () => {
-            const saveChangesPromise = offlineObjectStorage.saveChanges();
-            viewManager.notifyProgress(saveChangesPromise, "Changes saved", "All changes were pushed to server");
+            await offlineObjectStorage.saveChanges();
+            viewManager.notifySuccess("Changes saved", "All changes were pushed to server");
         });
     }
 }
