de48e4574f99e907f8c4d208b55a91da2b33ba46
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2019-11-17 13:50:02 -0800

    Added jsdocs to navigation item model.

--- src/navigation/navigationItemModel.ts
+++ src/navigation/navigationItemModel.ts
@@ -1,10 +1,38 @@
 ﻿export class NavigationItemModel {
+    /**
+     * Unique identifier.
+     */
     public key: string;
+
+    /**
+     * Label.
+     */
     public label: string;
+
+    /**
+     * Target key.
+     */
     public targetKey: string;
+
+    /**
+     * Target URL.
+     */
     public targetUrl?: string;
+
+    /**
+     * Child navigation items.
+     */
     public nodes: NavigationItemModel[];
+
+    /**
+     * Indicates if item should be highlighted as actve.
+     */
     public isActive?: boolean;
+
+    /**
+     * Level in navigation tree.
+     */
+    public level?: number;
 
     constructor() {
         this.nodes = [];
