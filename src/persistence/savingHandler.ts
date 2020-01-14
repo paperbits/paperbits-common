a3f9fd1674939cd55d5a505167fb0e8db068196a
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-01-14 11:19:47 -0800

    Returned changes saving notifications to SaveHandler.

--- src/persistence/savingHandler.ts
+++ src/persistence/savingHandler.ts
@@ -6,7 +6,12 @@ import { OfflineObjectStorage } from ".";
 export class SavingHandler {
     constructor(eventManager: EventManager, offlineObjectStorage: OfflineObjectStorage, viewManager: ViewManager) {
         eventManager.addEventListener("onSaveChanges", async () => {
+            if (!offlineObjectStorage.hasUnsavedChanges()) {
+                return;
+            }
+
             await offlineObjectStorage.saveChanges();
+            viewManager.notifySuccess("Changes saved", "All changes saved successfully.");
         });
     }
 }
