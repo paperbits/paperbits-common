0b5f568ea462ab898e8bae73998c3601e4ce81d4
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-02-17 22:17:18 -0800

    Added media query generation for global JSS.

--- src/styles/styleMediaQuery.ts
+++ src/styles/styleMediaQuery.ts
@@ -3,14 +3,28 @@ import { Style } from "./styles";
 export class StyleMediaQuery {
     public minWidth: number;
     public styles: Style[];
+    public globalStyles: Style[];
 
     constructor(minWidth: number) {
         this.minWidth = minWidth;
         this.styles = [];
+        this.globalStyles = [];
     }
 
     public toJssString(): string {
-        const styles = this.styles.map(style => style.toJssString()).join();
-        return `"@media(min-width:${this.minWidth}px)":{${styles}}`;
+        const stylesJss = this.styles.map(style => style.toJssString()).join();
+        const globalStylesJss = this.globalStyles.map(style => style.toJssString()).join();
+
+        const general = !!stylesJss
+            ? stylesJss
+            : "";
+
+        const global = !!globalStylesJss
+            ? `"@global": {${globalStylesJss}}`
+            : "";
+
+        const jssString = `"@media(min-width:${this.minWidth}px)":{${[global, general].filter(x => !!x).join(",")}}`;
+
+        return jssString;
     }
 }
--- src/styles/styleSheet.ts
+++ src/styles/styleSheet.ts
@@ -1,4 +1,3 @@
-import { BreakpointValues } from "./breakpoints";
 import { StyleMediaQuery } from "./styleMediaQuery";
 import { Style } from "./styles";
 import { FontFace } from "./FontFace";
@@ -17,33 +16,5 @@ export class StyleSheet {
         this.globalStyles = [];
         this.mediaQueries = [];
         this.fontFaces = [];
-    }
-
-    private flattenMediaQueries(styles: Style[]): StyleMediaQuery[] {
-        const nestedMediaQueries = styles.map(x => x.nestedMediaQueries);
-        const flattenNestedMediaQueries = nestedMediaQueries.reduce((acc, next) => acc.concat(next), []);
-        const groupedMediaQueries = [];
-
-        for (const breakpointMinWidth of Object.values(BreakpointValues)) {
-            const mediaQuery = new StyleMediaQuery(breakpointMinWidth);
-
-            flattenNestedMediaQueries
-                .filter(x => x.minWidth === breakpointMinWidth)
-                .forEach(x => mediaQuery.styles.push(...x.styles));
-
-            groupedMediaQueries.push(mediaQuery);
-        }
-
-        return groupedMediaQueries;
-    }
-
-    public toJssString(): string {
-        const fontFacesJssString = `"@font-face":[${this.fontFaces.map(x => x.toJssString()).join(",")}]`;
-        const stylesJssString = this.styles.map(style => style.toJssString()).filter(x => !!x).join(",");
-        const mediaQueries = this.flattenMediaQueries(this.styles);
-        const mediaQueriesJssString = mediaQueries.map(x => x.toJssString()).filter(x => !!x).join(",");
-        const result = [fontFacesJssString, stylesJssString, mediaQueriesJssString].filter(x => !!x).join(",");
-
-        return `{${result}}`;
     }
 }
