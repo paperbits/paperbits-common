bcddbb5454a4b0201661226a593a51c956aeb137
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-04-09 16:51:20 -0700

    Added promise rejection capturing to unhandled error handler.

--- src/errors/unhandledErrorHandler.ts
+++ src/errors/unhandledErrorHandler.ts
@@ -1,15 +1,24 @@
 import { EventManager } from "../events";
 import { ViewManager } from "../ui";
+import { Logger } from "../logging";
 
 export class UnhandledErrorHandler {
     constructor(
         private readonly eventManager: EventManager,
-        private readonly viewManager: ViewManager
+        private readonly viewManager: ViewManager,
+        private readonly logger: Logger
     ) {
         this.eventManager.addEventListener("onError", this.handlerError.bind(this));
+        window.addEventListener("unhandledrejection", this.handlerPromiseRejection.bind(this), true);
     }
 
     public handlerError(event: ErrorEvent): void {
         this.viewManager.notifyError("Oops, something went wrong.", "We are unable to complete your operation this time. Please try again later.");
+        this.logger.traceError(event.error);
+    }
+
+    public handlerPromiseRejection(event: PromiseRejectionEvent): void {
+        this.viewManager.notifyError("Oops, something went wrong.", "We are unable to complete your operation this time. Please try again later.");
+        this.logger.traceError(event.reason);
     }
 }
