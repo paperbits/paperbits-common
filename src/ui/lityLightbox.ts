d94e7acdaac4ec85e566e5b77751751fc959d84f
Tuan Nguyen <atuannguyen1101@gmail.com>
2020-04-28 19:12:35 -0400

    Clean up code

--- src/ui/lityLightbox.ts
+++ src/ui/lityLightbox.ts
@@ -5,28 +5,11 @@ export class LityLightbox implements ILightbox {
 
     public videoType: Set<string>;
 
-    constructor() {
-        this.videoType = new Set(["mpeg", "mp4", "quicktime", "x-ms-wmv", "x-msvideo", "x-flv", "webm"])
-    }
-
     show(url: string, fileName: string): void {
-        let lightbox;
-        
-        let splitFileName = fileName.split(".");
-        const fileType = splitFileName[splitFileName.length - 1]
-
-        // if ( this.videoType.has(fileType) ) {
-        //     lightbox = basicLightbox.create(`
-        //         <div class="lightbox-title">${fileName}</div>
-        //         <video src="${url}" controls>
-        //         </video>
-        //     `)
-        // } else {
-            lightbox = basicLightbox.create(`
-                <div class="lightbox-title">${fileName}</div>
-                <img class="lightbox-img" src="${url}">
-            `)
-        // }
+        const lightbox = basicLightbox.create(`
+            <div class="lightbox-title">${fileName}</div>
+            <img class="lightbox-img" src="${url}">
+        `)
         lightbox.show()
     }
 }
