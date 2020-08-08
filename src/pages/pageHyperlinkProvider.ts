e31c8b60900dea7b44bf86ec1e737a7a5bd8fadb
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-08-08 13:40:27 -0700

    Replaced Pages icon on hyperlink selector to match with main menu.

--- src/pages/pageHyperlinkProvider.ts
+++ src/pages/pageHyperlinkProvider.ts
@@ -5,7 +5,7 @@ import { HyperlinkModel } from "../permalinks/hyperlinkModel";
 export class PageHyperlinkProvider implements IHyperlinkProvider {
     public readonly name: string = "Pages";
     public readonly componentName: string = "page-selector";
-    public readonly iconClass: string = "paperbits-icon paperbits-single-content-03";
+    public readonly iconClass: string = "paperbits-icon paperbits-menu-4";
 
     public canHandleHyperlink(contentItemKey: string): boolean {
         return contentItemKey.startsWith("pages/");
