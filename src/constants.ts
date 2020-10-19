1fb928b9946b5ebf4b2d7e813b45b625cab3b782
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-10-19 12:22:17 -0700

    Fixed typo.

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const localePrefix = "locales";
 export const defaultPageSize = 30;
-export const iconsFontsLibraryUrl = "/data/icons-fonts.json";
+export const iconsFontsLibraryUrl = "/data/icon-fonts.json";
 export const blockSnippetsLibraryUrl = "/data/block-snippets.json";
 export const styleSnippetsLibraryUrl = "/data/style-snippets.json";
