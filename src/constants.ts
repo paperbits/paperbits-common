6951a2326d40ff432ece1e95d62a01a8effb1bd5
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-11-07 19:44:54 -0800

    Fixes and optimizations for page publishing.

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,6 @@
 export const localePrefix = "locales";
 export const defaultPageSize = 30;
+export const maxParallelPublisingTasks = 30;
 export const iconsFontsLibraryUrl = "/data/icon-fonts.json";
 export const blockSnippetsLibraryUrl = "/data/block-snippets.json";
 export const styleSnippetsLibraryUrl = "/data/style-snippets.json";
