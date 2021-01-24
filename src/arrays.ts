cd2a6ac8558e61923af639fc3fd48cab46099c4b
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2021-01-23 16:06:57 -0800

    Added "distinct" operation to Arrays utility.

--- src/arrays.ts
+++ src/arrays.ts
@@ -2,6 +2,19 @@
  * Converts array-like object into actual array.
  * @param arrayLikeObject 
  */
-export function coerce<T>(arrayLikeObject): T[] {
+export function coerce<T>(arrayLikeObject: any): T[] {
     return Array.prototype.slice.call(arrayLikeObject);
+}
+
+/**
+ * Removes duplicate values from specified array.
+ */
+export function distinct<T>(arr: T[]): T[] {
+    const uniq: any = [];
+
+    for (const val of arr) {
+        uniq[val] = 0;
+    }
+
+    return <any>Object.keys(uniq);
 }
