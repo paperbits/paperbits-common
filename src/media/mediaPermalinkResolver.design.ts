f0578011359ca017090671aa538fda54cadd062e
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2021-01-28 09:53:26 -0800

    Fixed media permalink resolver for design-time.

--- src/media/mediaPermalinkResolver.design.ts
+++ src/media/mediaPermalinkResolver.design.ts
@@ -1,16 +1,20 @@
 import { IMediaService } from "./IMediaService";
-import { IPermalinkResolver } from "../permalinks";
+import { HyperlinkModel, IPermalinkResolver } from "../permalinks";
 import { IBlobStorage } from "../persistence";
+import { HyperlinkContract } from "../editing";
+import { MediaContract } from "./mediaContract";
 
 
 export class MediaPermalinkResolver implements IPermalinkResolver {
+    protected mediaPath: string = "uploads/";
+
     constructor(
         private readonly mediaService: IMediaService,
         private readonly blobStorage: IBlobStorage
     ) { }
 
     public canHandleTarget(targetKey: string): boolean {
-        return targetKey.startsWith("uploads/");
+        return targetKey.startsWith(this.mediaPath);
     }
 
     public async getUrlByTargetKey(mediaKey: string): Promise<string> {
@@ -39,5 +43,50 @@ export class MediaPermalinkResolver implements IPermalinkResolver {
         }
 
         return mediaUrl;
+    }
+    
+    private async getHyperlink(mediaContract: MediaContract, hyperlinkContract?: HyperlinkContract): Promise<HyperlinkModel> {
+        const hyperlinkModel = new HyperlinkModel();
+        hyperlinkModel.targetKey = mediaContract.key;
+        hyperlinkModel.href = mediaContract.permalink;
+        hyperlinkModel.title = mediaContract.fileName || mediaContract.permalink;
+
+        if (hyperlinkContract) {
+            hyperlinkModel.target = hyperlinkContract.target;
+            hyperlinkModel.anchor = hyperlinkContract.anchor;
+            hyperlinkModel.anchorName = hyperlinkContract.anchorName;
+        }
+
+        return hyperlinkModel;
+    }
+
+    public async getHyperlinkFromContract(hyperlinkContract: HyperlinkContract, locale?: string): Promise<HyperlinkModel> {
+        if (!hyperlinkContract.targetKey) {
+            throw new Error("Target key cannot be null or empty.");
+        }
+
+        if (!hyperlinkContract.targetKey.startsWith(this.mediaPath)) {
+            return null;
+        }
+
+        let hyperlinkModel: HyperlinkModel;
+
+        if (hyperlinkContract.targetKey) {
+            const mediaContract = await this.mediaService.getMediaByKey(hyperlinkContract.targetKey);
+
+            if (mediaContract) {
+                return this.getHyperlink(mediaContract, hyperlinkContract);
+            }
+        }
+
+        hyperlinkModel = new HyperlinkModel();
+        hyperlinkModel.title = "Unset link";
+        hyperlinkModel.target = hyperlinkContract.target;
+        hyperlinkModel.targetKey = hyperlinkContract.targetKey;
+        hyperlinkModel.href = "#";
+        hyperlinkModel.anchor = hyperlinkContract.anchor;
+        hyperlinkModel.anchorName = hyperlinkContract.anchorName;
+
+        return hyperlinkModel;
     }
 }
