020f5c5fafbd87972aed9ee348c3745f2a7ddac8
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2021-03-12 17:59:00 -0800

    Accessibility fixes.

--- src/html.ts
+++ src/html.ts
@@ -13,3 +13,35 @@ export function closest(node: Node, predicate: (node: Node) => boolean): Node {
     }
     while (node);
 }
+
+export enum AriaAttributes {
+    /**
+     * Indicates whether the element, or another grouping element it controls, is currently expanded or collapsed.
+     */
+    expanded = "aria-expanded",
+
+    /**
+     * Defines a string value that labels the current element.
+     */
+    label = "aria-label",
+
+    /**
+     * Identifies the element (or elements) whose contents or presence are controlled by the current element.
+     */
+    controls = "aria-controls",
+
+    /**
+     * Indicates whether the element is exposed to an accessibility API.
+     */
+    hidden = "aria-hidden",
+
+    /**
+     * Indicates the current “selected” state of various widgets. See related aria-checked and aria-pressed.
+     */
+    selected = "aria-selected",
+
+    /**
+     * Indicates the availability and type of interactive popup element, such as menu or dialog, that can be triggered by an element.
+     */
+    hasPopup = "aria-haspopup",
+}
--- src/ui/balloons/balloonActivationOptions.ts
+++ src/ui/balloons/balloonActivationOptions.ts
@@ -1,4 +1,4 @@
 export enum BalloonActivationOptions {
-    click = "click",
+    clickOrKeyDown = "clickOrKeyDown",
     hoverOrFocus = "hoverOrFocus"
 }
