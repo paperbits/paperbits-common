996b77507f45c261634366dc6032a56f9c8b54de
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2021-03-15 21:47:58 -0700

    Added jsdocs to FontFace class.

--- src/styles/fontFace.ts
+++ src/styles/fontFace.ts
@@ -1,6 +1,21 @@
 export class FontFace {
+    /**
+     * Font family name, e.g. `Quick Sand`.
+     */
     public fontFamily: string;
+
+    /**
+     * Font face source file, e.g. `/fonts/QuickSand-Regular.ttf`.
+     */
     public source: string;
+
+    /**
+     * Font face attributed style, e.g. `italic`.
+     */
     public fontStyle: string;
+
+    /**
+     * Font face attributed weight, e.g. `bold`.
+     */
     public fontWeight: number | string;
 }
