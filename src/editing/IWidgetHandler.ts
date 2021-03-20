2cd073155274eb798da143594b4bc349b16fa6fc
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2021-03-20 09:42:32 -0700

    Fixed typo.

--- src/editing/IWidgetHandler.ts
+++ src/editing/IWidgetHandler.ts
@@ -4,7 +4,7 @@ import { DragSession } from "../ui/draggables";
 
 
 /**
- * Handlers giving the editor required context to manupulate the widget. For example,
+ * Handlers give the editor required context to manupulate the widget. For example,
  * it describes how the widget gets created, how it responds to drag'n'drop events,
  * what contextual commands is supports, etc.
  */
