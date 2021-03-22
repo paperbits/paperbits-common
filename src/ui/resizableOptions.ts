1fcbe9e34bd0e10ddb81a7391ead6889440bcc7c
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2021-03-21 23:11:16 -0700

    Added jsdocs to common interfaces.

--- src/ui/resizableOptions.ts
+++ src/ui/resizableOptions.ts
@@ -3,7 +3,19 @@ export interface ResizableOptions {
      * Allowed values: "none", "vertically", "horizontally".
      */
     directions: string;
+
+    /**
+     * Callback delegate invoked when element gets resized.
+     */
     onresize?: () => void;
+
+    /**
+     * Initial width of resizable element.
+     */
     initialWidth?: number;
+
+    /**
+     * Initial height of resizable element.
+     */
     initialHeight?: number;
 }
