8717e81a5992fe05deceb29a4a0a84e68f50059d
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2021-04-27 13:21:26 -0700

    Added popup permalink resolver to index file.

--- src/popups/index.ts
+++ src/popups/index.ts
@@ -2,3 +2,4 @@ export * from "./IPopupService";
 export * from "./popupContract";
 export * from "./popupHyperlinkProvider";
 export * from "./popupService";
+export * from "./popupPermalinkResolver";
