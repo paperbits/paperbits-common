c8693e8c2aaaa8b985f5fdfe397ce0fddc9282ee
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2021-08-03 01:01:14 -0700

    Added fallback flag to style rule.

--- src/styles/styleRule.ts
+++ src/styles/styleRule.ts
@@ -1,10 +1,21 @@
+/**
+ * Style rule.
+ */
 export class StyleRule {
     public readonly name: string;
     public readonly value: any;
+    public readonly fallback: boolean;
 
-    constructor(name: string, value: any) {
+    /**
+     * Style rule.
+     * @param name {string} Style rule name, i.e. `background-color`.
+     * @param value {string} Style rule value, i.e. `#ffffff`.
+     * @param fallback {boolean} Indicates that this is a fallback rule.
+     */
+    constructor(name: string, value: any, fallback: boolean = false) {
         this.name = name;
         this.value = value;
+        this.fallback = fallback;
     }
 
     public toJssString(): string {
