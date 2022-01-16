45f0b3f1287d6fe5c01a6b860634a0ffdc7c6184
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2022-01-15 20:02:32 -0800

    Fixed KO bootstrapping.

--- src/ko/utils.ts
+++ src/ko/utils.ts
@@ -1,5 +1,9 @@
 import * as ko from "knockout";
 
+/**
+ * Attaches the designer application to specified DOM element.
+ * @param element HTML element the designer app to be attached to.
+ */
 export function attachDesignerTo(element: HTMLElement): void {
     ko.applyBindingsToNode(element, { component: "view-manager" }, null);
 }
