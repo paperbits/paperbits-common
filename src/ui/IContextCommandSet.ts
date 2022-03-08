713446afa7ffe07f2f93418e9383a384ea813648
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2022-03-07 18:56:26 -0800

    tmp

--- src/ui/IContextCommandSet.ts
+++ src/ui/IContextCommandSet.ts
@@ -11,6 +11,7 @@ export interface IContextCommandSet {
 
 export interface IContextCommand {
     name?: string;
+    displayName?: string;
     callback?: () => void;
     component?: IComponent;
     tooltip?: string;
