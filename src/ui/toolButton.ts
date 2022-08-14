46ac44b7396a8ddea6b01441640081005ac50de2
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2022-08-14 16:18:56 -0700

    Extended ToolButton configuration with balloon component option.

--- src/ui/toolButton.ts
+++ src/ui/toolButton.ts
@@ -1,6 +1,31 @@
+import { IComponent } from "./IComponent";
+
+/**
+ * Tool button configuration.
+ */
 export interface ToolButton {
+    /**
+     * Icon CSS class, e.g. `widget-icon widget-icon-form`.
+     */
     iconClass: string;
+
+    /**
+     * Title.
+     */
     title: string;
+
+    /**
+     * Tooltip that appears on mouse hovering or getting focus.
+     */
     tooltip?: string | (() => string);
-    onActivate: () => void;
+
+    /**
+     * Activation handler.
+     */
+    onActivate?: () => void;
+
+    /**
+     * A component shown in the balloon view.
+     */
+    component?: IComponent;
 }
