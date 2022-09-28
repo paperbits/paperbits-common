60bdb0feef52fde5a46db6e2e34951941f150546
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2022-09-27 21:41:50 -0700

    Marked canHandleContract and canHandleModel methods deprecated.

--- src/editing/IModelBinder.ts
+++ src/editing/IModelBinder.ts
@@ -7,27 +7,27 @@ import { Contract } from "../contract";
  */
 export interface IModelBinder<TModel> {
     /**
-     * Determines if specified contract can be converted into model by this model binder.
-     * @param contract {Contract} Contract.
+     * @deprecated Determines if specified contract can be converted into model by this model binder.
+     * @param contract - Contract.
      */
-    canHandleContract(contract: Contract): boolean;
+    canHandleContract?(contract: Contract): boolean;
 
     /**
-     * Determines if specified model can be converted into contract by this model binder.
-     * @param model {Object} Model.
+     * @deprecated Determines if specified model can be converted into contract by this model binder.
+     * @param model - Model.
      */
-    canHandleModel(model: TModel): boolean;
+    canHandleModel?(model: TModel): boolean;
 
     /**
      * Converts a model to a contract.
-     * @param model {TModel} Model.
+     * @param model - Model.
      */
     modelToContract(model: TModel): any;
 
     /**
      * Converts contract to model.
-     * @param contract {Contract} Contract.
-     * @param bindingContext {Bag<T>} Binding context.
+     * @param contract - Contract.
+     * @param bindingContext - Binding context.
      */
     contractToModel(contract: any, bindingContext?: Bag<any>): Promise<any>;
 }
