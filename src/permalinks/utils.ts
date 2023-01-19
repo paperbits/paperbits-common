a55fa7105ab4b76fc4fcdb1eaf1dc783bbae5a22
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2023-01-18 18:44:12 -0800

    Add the regexp check to permalink normalization util.

--- src/permalinks/utils.ts
+++ src/permalinks/utils.ts
@@ -1,10 +1,18 @@
-export function normalizePermalink(permalink: string): string {
+import { RegExps } from "../regexps";
+
+export function validateAndNormalizePermalink(permalink: string): string {
     if (!permalink.startsWith("/")) {
         permalink = `/${permalink}`;
     }
 
-    if (permalink.endsWith("/")) {
+    if (permalink.length > 1 && permalink.endsWith("/")) {
         permalink = permalink.substring(0, permalink.length - 1)
+    }
+
+    const isPermalinkValid = RegExps.permalink.test(permalink);
+
+    if (!isPermalinkValid) {
+        throw new Error(`The permalink is invalid: "${permalink}".`);
     }
 
     return permalink;
