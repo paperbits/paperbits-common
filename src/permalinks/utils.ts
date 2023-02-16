25bedb12491c57059bc50b41f81f5cd073ee22c8
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2023-02-15 22:05:46 -0800

    Adjusted publishing behavior for resources with invalid permalink.

--- src/permalinks/utils.ts
+++ src/permalinks/utils.ts
@@ -1,18 +1,10 @@
-import { RegExps } from "../regexps";
-
-export function validateAndNormalizePermalink(permalink: string): string {
+export function normalizePermalink(permalink: string): string {
     if (!permalink.startsWith("/")) {
         permalink = `/${permalink}`;
     }
 
     if (permalink.length > 1 && permalink.endsWith("/")) {
         permalink = permalink.substring(0, permalink.length - 1)
-    }
-
-    const isPermalinkValid = RegExps.permalink.test(permalink);
-
-    if (!isPermalinkValid) {
-        throw new Error(`The permalink is invalid: "${permalink}".`);
     }
 
     return permalink;
