cd2e10da8ef28f65d0c39859919e4176b775b14d
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2023-06-10 18:07:59 -0700

    Added jsdocs.

--- src/http/httpClient.ts
+++ src/http/httpClient.ts
@@ -1,6 +1,12 @@
 import { HttpResponse } from "./httpResponse";
 import { HttpRequest } from "./httpRequest";
 
+/**
+ * Basic HTTP client.
+ */
 export interface HttpClient {
+    /**
+     * Sends HTTP request.
+     */
     send<T>(request: HttpRequest): Promise<HttpResponse<T>>;
 }
--- src/http/httpRequest.ts
+++ src/http/httpRequest.ts
@@ -1,8 +1,26 @@
 ﻿import { HttpHeader } from "./httpHeader";
 
+/**
+ * HTTP request.
+ */
 export interface HttpRequest {
+    /**
+     * Request URL, e.g. `https://httpbin.org/get`;
+     */
     url: string;
+
+    /**
+     * HTTP method, e.g. `GET`.
+     */
     method?: string;
+
+    /**
+     * HTTP headers, e.g. `application/json`.
+     */
     headers?: HttpHeader[];
+
+    /**
+     * HTTP request body (optional).
+     */
     body?: any;
 }
